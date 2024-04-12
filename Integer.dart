9509c1e140ea219c54ca14da0fb6922ebbc5294d
MUSYOKIPeter <musyoki.petern@students.uonbi.ac.ke>
2024-04-12 23:11:12 +0300

    first commit

---
+++ Integer.dart
@@ -0,0 +1,21 @@
+void main() {
+  // integers
+  int age = 25;
+  int yob = 1768;
+  print(yob);
+
+  // doubles
+  double currency = 45.87;
+  print(currency);
+
+  // Uncommented lines
+  double gpa = 3.9;
+  print("My age is $age");
+
+  print("My gpa is $gpa");
+  print(4 / 8);
+  print(gpa * age);
+  print(gpa / age);
+  print(gpa % age); // modulo
+  print(10 % 2);
+}
---
+++ main.dart
@@ -0,0 +1,7 @@
+void main() {
+  String name = "MUSYOKI Peter";
+  int age = 25;
+  double unit = 4.1;
+  print(
+      "My name is $name"); // Changed "My age is" to "My name is" and used string interpolation
+}
